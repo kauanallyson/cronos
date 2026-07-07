72729181e9495edd058bae5c2c36d35f62acf88f
Kauan <kauanallysonrp@gmail.com>
2026-07-07 11:57:54 -0300

    feat: add timer

--- src/main.c
+++ src/main.c
@@ -1,35 +1,35 @@
-#include <stdio.h>
-
 #include "raylib.h"
 
 #define RESOURCES "../resources/"
-#define DEFAULT_FONT RESOURCES "Audiowide-Regular.ttf"
 #define MENU_MUSIC RESOURCES "awesomeness.wav"
 
-static const Color ZOZBLACK = {
-	0x18,
-	0x18,
-	0x18,
-	0xFF};
+#define ZOZBLACK (Color){24, 24, 24, 255}
+#define MUSGO_GREEN (Color){10, 95, 30, 255}
 
 int main(void)
 {
 	const char *wTitle = "cronos";
 	const Vector2 wSizes = {800, 600};
+	const Vector2 center = {.x = wSizes.x / 2.0f, .y = wSizes.y / 2.0f};
 
 	InitWindow(wSizes.x, wSizes.y, wTitle);
 	SetTargetFPS(60);
 	ChangeDirectory(GetApplicationDirectory()); // set relative path
 
-	const char *text = "Olá mundo!";
+	const char *text = "cronos";
 	const float fontSize = 40.0f;
 	const float spacing = 1.0f;
-	const Font font = LoadFontEx(DEFAULT_FONT, fontSize, 0, 255);
+	const Font font = GetFontDefault();
 
 	InitAudioDevice();
 	Music music = LoadMusicStream(MENU_MUSIC);
 	PlayMusicStream(music);
 	float timePlayed = 0.0f; // [0.0f..1.0f]
+
+	const int segments = 36;
+	const float radius = 100.0f;
+	const float startAngle = 270.0f; // 12horas
+	float endAngle = timePlayed * 360.0f + startAngle;
 
 	while (!WindowShouldClose())
 	{
@@ -37,18 +37,21 @@ int main(void)
 		timePlayed = GetMusicTimePlayed(music) / GetMusicTimeLength(music);
 		if (timePlayed > 1.0f)
 		{
-			timePlayed = 0.0f;
+			timePlayed = 0.0f; // loop
 		}
+
+		endAngle = (timePlayed * 360.0f) + startAngle;
 
 		Vector2 tSizes = MeasureTextEx(font, text, fontSize, spacing);
 		Vector2 tPos = {
-			.x = (wSizes.x / 2.0f) - (tSizes.x / 2.0f),
-			.y = (wSizes.y / 2.0f) - (tSizes.y / 2.0f)};
+			.x = center.x - (tSizes.x / 2.0f),
+			.y = (tSizes.y / 2.0f)};
 
 		BeginDrawing();
 		{
 			ClearBackground(ZOZBLACK);
 			DrawTextEx(font, text, tPos, fontSize, spacing, RAYWHITE);
+			DrawCircleSector(center, radius, startAngle, endAngle, segments, MUSGO_GREEN);
 		}
 		EndDrawing();
 	}
@@ -57,6 +60,7 @@ int main(void)
 	CloseAudioDevice();
 
 	UnloadFont(font);
+
 	CloseWindow();
 	return 0;
 }
