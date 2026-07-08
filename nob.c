391f145622622885ab8a653d203e7f52d9937531
Kauan <kauanallysonrp@gmail.com>
2026-07-07 23:51:09 -0300

    feat: add cover image

--- nob.c
+++ nob.c
@@ -2,26 +2,26 @@
 #define NOB_WARN_DEPRECATED
 #include "nob.h"
 
-#define BUILD_FOLDER "build/"
-#define SRC_FOLDER "src/"
-#define RAYPATH "raylib-6.0_linux_amd64/"
+#define BUILD_DIR "build/"
+#define SRC_DIR "src/"
+#define RAYPATH "./raylib-6.0_linux_amd64/"
 
 int main(int argc, char **argv)
 {
     NOB_GO_REBUILD_URSELF(argc, argv);
 
-    if (!nob_mkdir_if_not_exists(BUILD_FOLDER))
+    if (!nob_mkdir_if_not_exists(BUILD_DIR))
     {
-        nob_log(NOB_ERROR, "Could not create build folder '%s'", BUILD_FOLDER);
+        nob_log(NOB_ERROR, "Could not create build folder '%s'", BUILD_DIR);
         return 1;
     }
 
     Nob_Cmd cmd = {0};
-    nob_cmd_append(&cmd, "gcc", "-Wall", "-Wextra",
-                   "-o",
-                   BUILD_FOLDER "cronos", SRC_FOLDER "main.c");
+    nob_cmd_append(&cmd, "gcc", "-Wall", "-Wextra", "-ggdb");
 
+    nob_cmd_append(&cmd, "-I.");
     nob_cmd_append(&cmd, "-I" RAYPATH "include");
+    nob_cmd_append(&cmd, "-o", BUILD_DIR "cronos", SRC_DIR "main.c");
     nob_cmd_append(&cmd, "-L" RAYPATH "lib");
     nob_cmd_append(&cmd, "-l:libraylib.a", "-lm", "-lX11");
 
--- src/main.c
+++ src/main.c
@@ -1,63 +1,84 @@
 #include "raylib.h"
+#define NOB_IMPLEMENTATION
 
-#define RESOURCES "../resources/"
-#define MENU_MUSIC RESOURCES "awesomeness.wav"
+#define RESOURCES_DIR "../resources/"
+#define BG_MUSIC RESOURCES_DIR "On & On NCS.mp3"
+#define COVER RESOURCES_DIR "cover.png"
 
 #define ZOZBLACK (Color){24, 24, 24, 255}
 #define MUSGO_GREEN (Color){10, 95, 30, 255}
 
 int main(void)
 {
-	const char *W_TITLE = "cronos";
-	const Vector2 W_SIZE = {800, 600};
-	const Vector2 SCREEN_CENTER = {.x = W_SIZE.x / 2.0f, .y = W_SIZE.y / 2.0f};
+	const char *wTitle = "cronos";
+	Vector2 wSize = {800, 600};
+	Vector2 wCenter = {.x = wSize.x / 2.0f, .y = wSize.y / 2.0f};
 
-	InitWindow(W_SIZE.x, W_SIZE.y, W_TITLE);
+	InitWindow((int)wSize.x, (int)wSize.y, wTitle);
 	SetTargetFPS(60);
 	ChangeDirectory(GetApplicationDirectory()); // set relative path
 
-	const char *CRONOS_LABEL = "cronos";
-	const Font FONT = GetFontDefault();
-	const float FONT_SIZE = 40.0f;
-	const float FONT_SPACING = 1.0f;
+	// label
+	const char *label = "cronos";
+	Font font = GetFontDefault();
+	const float fontSize = 40.0f;
+	const float fontSpacing = 1.0f;
+	const Vector2 textSize = MeasureTextEx(font, label, fontSize, fontSpacing);
+	const Vector2 textPos = {
+		.x = wCenter.x - (textSize.x / 2.0f),
+		.y = (textSize.y / 2.0f)};
 
-	const Vector2 TEXT_SIZE = MeasureTextEx(FONT, CRONOS_LABEL, FONT_SIZE, FONT_SPACING);
-	const Vector2 TEXT_POS = {
-		.x = SCREEN_CENTER.x - (TEXT_SIZE.x / 2.0f),
-		.y = (TEXT_SIZE.y / 2.0f)};
-
+	// music
 	InitAudioDevice();
-	const Music MUSIC = LoadMusicStream(MENU_MUSIC);
-	PlayMusicStream(MUSIC);
+	Music music = LoadMusicStream(BG_MUSIC);
+	if (!IsMusicValid(music))
+	{
+		TraceLog(LOG_WARNING, "Failed to load music: %s", BG_MUSIC);
+	}
+	PlayMusicStream(music);
 	bool pause = false;
-	float timePlayed = 0.0f; // [0.0f..1.0f]
-	float volume = 0.8f;	 // [0.0f..1.0f]
+	float volume = 0.5f;						// [0.0f..1.0f]
+	float timePlayed = 0.0f;					// [0.0f..1.0f]
+	float musicLen = GetMusicTimeLength(music); // Get music time length (in seconds)
+	float secsLeft = musicLen - timePlayed;
+	const char *secsLabel = TextFormat("%02.02f s", secsLeft);
+	SetMusicVolume(music, volume);
 	const float DELTA_VOLUME = 0.025f;
-	SetMusicVolume(MUSIC, volume);
+	Vector2 secsPos = {.x = textPos.x, .y = textPos.y + textSize.y};
 
-	const int SEGMENTS = 36;
-	const float RADIUS = 100.0f;
-	const float START_ANGLE = 270.0f;					// 12horas
-	float endAngle = timePlayed * 360.0f + START_ANGLE; // offset
+	Image image = LoadImage(COVER);
+	if (!IsImageValid(image))
+	{
+		TraceLog(LOG_WARNING, "Failed to load image: %s", COVER);
+	}
+	Rectangle cropRect = {
+		.x = (image.width - 320.0f) / 2.0f,
+		.y = (image.height - 320.0f) / 2.0f,
+		.width = 320.0f,
+		.height = 320.0f};
+	ImageCrop(&image, cropRect);
+	Rectangle imagePos = {.x = wCenter.x - 320.0f / 2.0f, .y = wCenter.y - 320.0f / 2.0f, .width = 320.0f, .height = 320.0f};
+	Texture2D texture = LoadTextureFromImage(image);
+	UnloadImage(image); // depois de gerar a textura pode dar free na imagem
 
 	// lb = LoadingBar
-	const float NINTH_Y = W_SIZE.y * 0.9f;
-	const float LB_THICKNESS = 5.0f;
-	const Vector2 LB_START_POS = {.x = W_SIZE.x * 0.1f, .y = NINTH_Y};
-	const Vector2 LB_END_POS = {.x = W_SIZE.x * 0.9f, .y = NINTH_Y};
-	Vector2 lbCurrPos = {.x = LB_START_POS.x, .y = NINTH_Y};
+	float lbYPos = wSize.y * 0.9f;
+	float thickness = 10.0f;
+	Vector2 lbStartPos = {.x = wSize.x * 0.1f, .y = lbYPos};
+	Vector2 lbEndPos = {.x = wSize.x * 0.9f, .y = lbYPos};
+	Vector2 lbCurrPos = {.x = lbStartPos.x, .y = lbYPos};
 
 	while (!WindowShouldClose())
 	{
-		UpdateMusicStream(MUSIC);
+		UpdateMusicStream(music);
 
 		if (IsKeyPressed(KEY_SPACE) || IsMouseButtonPressed(MOUSE_BUTTON_LEFT))
 		{
 			pause = !pause;
 			if (pause)
-				PauseMusicStream(MUSIC);
+				PauseMusicStream(music);
 			else
-				ResumeMusicStream(MUSIC);
+				ResumeMusicStream(music);
 		}
 
 		if (IsKeyDown(KEY_UP))
@@ -65,50 +86,49 @@ int main(void)
 			volume += DELTA_VOLUME;
 			if (volume > 1.0f)
 				volume = 1.0f;
-			SetMusicVolume(MUSIC, volume);
+			SetMusicVolume(music, volume);
 		}
 		else if (IsKeyDown(KEY_DOWN))
 		{
 			volume -= DELTA_VOLUME;
 			if (volume < 0.0f)
 				volume = 0.0f;
-			SetMusicVolume(MUSIC, volume);
+			SetMusicVolume(music, volume);
 		}
 
 		if (IsKeyDown(KEY_LEFT_CONTROL) && IsKeyPressed(KEY_R))
 		{
-			StopMusicStream(MUSIC);
-			PlayMusicStream(MUSIC);
+			StopMusicStream(music);
+			PlayMusicStream(music);
 		}
 
-		endAngle = (timePlayed * 360.0f) + START_ANGLE;
-		lbCurrPos.x = timePlayed * (W_SIZE.x * 0.8f) + LB_START_POS.x;
-
-		timePlayed = GetMusicTimePlayed(MUSIC) / GetMusicTimeLength(MUSIC);
-
+		// timing
+		float timePlayedSecs = GetMusicTimePlayed(music);
+		timePlayed = timePlayedSecs / musicLen;
 		if (timePlayed > 1.0f)
-			timePlayed = 0.0f; // loop
+			timePlayed = 0.0f;
+		float secsLeft = musicLen - timePlayedSecs;
+		secsLabel = TextFormat("%02.02f s", secsLeft);
+		lbCurrPos.x = timePlayed * (wSize.x * 0.8f) + lbStartPos.x;
 
 		BeginDrawing();
 		{
 			ClearBackground(ZOZBLACK);
-			DrawTextEx(FONT, CRONOS_LABEL, TEXT_POS, FONT_SIZE, FONT_SPACING, RAYWHITE);
-
-			// pie loader
-			DrawCircleSector(SCREEN_CENTER, RADIUS, START_ANGLE, endAngle, SEGMENTS, MUSGO_GREEN);
-			DrawRingLines(SCREEN_CENTER, RADIUS - 1, RADIUS, START_ANGLE, endAngle, SEGMENTS, RAYWHITE);
+			DrawTextEx(font, label, textPos, fontSize, fontSpacing, RAYWHITE);
+			DrawTextEx(font, secsLabel, secsPos, fontSize, fontSpacing, RAYWHITE);
+			DrawTextureEx(texture, (Vector2){.x = imagePos.x, .y = imagePos.y}, 0, 1, WHITE);
 
 			// lb
-			DrawLineEx(LB_START_POS, LB_END_POS, LB_THICKNESS, RAYWHITE);
-			DrawLineEx(LB_START_POS, lbCurrPos, LB_THICKNESS, MUSGO_GREEN);
+			DrawLineEx(lbStartPos, lbEndPos, thickness, RAYWHITE);
+			DrawLineEx(lbStartPos, lbCurrPos, thickness, MUSGO_GREEN);
 		}
 		EndDrawing();
 	}
 
-	UnloadMusicStream(MUSIC);
+	UnloadMusicStream(music);
 	CloseAudioDevice();
 
-	UnloadFont(FONT);
+	UnloadTexture(texture);
 
 	CloseWindow();
 	return 0;
